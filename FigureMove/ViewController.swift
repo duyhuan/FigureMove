5985f044583afe62592b6028f165d1ec66e770be
duyhuan <huanhuan@Mac.local>
2017-03-28 00:23:17 +0700

    FigureMove using timer

--- FigureMove/ViewController.swift
+++ FigureMove/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var myView: UIView!
+    var timerMoveLeftToRight: Timer!
+    var timerMove: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myView = UIView(frame: CGRect(x: 25.0, y: 25.0, width: 50.0, height: 50.0))
+        myView.backgroundColor = UIColor.blue
+        view.addSubview(myView)
+        
+        moveLeftToRight()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func moveLeftToRight() {
+        timerMoveLeftToRight = Timer.scheduledTimer(timeInterval: 0.0002, target: self, selector: #selector(move), userInfo: nil, repeats: true)
     }
-
+    
+    func move() {
+        myView.center = CGPoint(x: myView.center.x + 1, y: myView.center.y)
+        if myView.center.x >= view.bounds.width - myView.bounds.width {
+            timerMoveLeftToRight.invalidate()
+            timerMove = Timer.scheduledTimer(timeInterval: 0.0002, target: self, selector: #selector(checkMove), userInfo: nil, repeats: true)
+        }
+        
+    }
+    
+    func checkMove() {
+        var xxx = CGFloat(1)
+        var yyy = CGFloat(1)
+        if myView.center.x >= view.bounds.width - myView.bounds.width {
+            xxx = 0
+            yyy = CGFloat(1)
+        }
+        if myView.center.y >= view.bounds.height - myView.bounds.height {
+            xxx = -CGFloat(1)
+            yyy = 0
+        }
+        if myView.center.x <= myView.bounds.width {
+            xxx = 0
+            yyy = -CGFloat(1)
+        }
+        if myView.center.y <= myView.bounds.height {
+            timerMove.invalidate()
+            moveLeftToRight()
+        }
+        myView.center = CGPoint(x: myView.center.x + xxx, y: myView.center.y + yyy)
+    }
 
 }
 
